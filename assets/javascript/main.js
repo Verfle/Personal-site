3a30b84638982c52325e0bb067dc47100ae57d9e
verflenic <96931728+Verfle@users.noreply.github.com>
2022-03-26 10:56:57 +0700

    Add files via upload

---
+++ assets/javascript/main.js
@@ -0,0 +1,27 @@
+// === social-links ===
+// const json = [
+// 	{
+// 		"id": "Github",
+// 		"url": "https://github.com/Verfle",
+// 		"icon": "fa-brands fa-github"
+// 	},
+// 	{
+// 		"id": "Facebook",
+// 		"url": "https://facebook.com/2006in",
+// 		"icon": "fa-brands fa-facebook"
+// 	},
+// 	{
+// 		"id": "Messenger",
+// 		"url": "https://facebook.com/2006in",
+// 		"icon": "fa-brands fa-facebook-messenger"
+// 	}
+// ]
+
+// json.forEach((obgs) => {
+// 	const social = document.getElementById('social-links');
+// 	social.innerHTML = `<a class="social-links" href="${obgs.url}"><i class="${obgs.icon}"></i></a>`
+// })
+
+// === age manager ===
+const age = document.getElementById('age');
+age.innerHTML = parseInt((Date.now() - new Date(2006, 11, 10).getTime()) / 31556952000);
