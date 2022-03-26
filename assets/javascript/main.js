9e7f91bc151d5dda18d2c0bc13cc166b43faf13b
verflenic <96931728+Verfle@users.noreply.github.com>
2022-03-26 19:05:52 +0700

    Add files via upload

---
+++ assets/javascript/main.js
@@ -0,0 +1,35 @@
+// === social-links ===
+const json = [
+	{
+		"id": "Github",
+		"url": "https://github.com/Verfle",
+		"icon": "fa-brands fa-github"
+	},
+	{
+		"id": "Facebook",
+		"url": "https://facebook.com/2006in",
+		"icon": "fa-brands fa-facebook"
+	},
+	{
+		"id": "Messenger",
+		"url": "https://facebook.com/2006in",
+		"icon": "fa-brands fa-facebook-messenger"
+	},
+	{
+		"id": "Source",
+		"url": "https://github.com/Verfle/Personal-site",
+		"icon": "fa-solid fa-code"
+	}
+]
+
+json.forEach((obgs) => {
+	const social = document.getElementById('social-links');
+	social.innerHTML += `<a target="_blank" class="social-links" href="${obgs.url}"><i class="${obgs.icon}"></i></a>`
+});
+// === content ===
+const name = document.getElementById('name');
+name.innerText = 'Verfle Thanyathep Gamber';
+
+// === age manager ===
+const age = document.getElementById('age');
+age.innerHTML = parseInt((Date.now() - new Date(2006, 11, 10).getTime()) / 31556952000);
